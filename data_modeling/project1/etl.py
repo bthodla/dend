9498988a0dea3b4133e72e9851e14b5e235881ad
Bhasker Thodla <bthodla@gmail.com>
2020-03-07 08:27:32 -0500

    Completed creating tables
    Completed creating tables

--- data_modeling/project1/etl.py
+++ data_modeling/project1/etl.py
@@ -47,8 +47,13 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
         songplay_data = 
