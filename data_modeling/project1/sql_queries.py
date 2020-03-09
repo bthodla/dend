be8909e6d77e3dc54b43210ec8e80da99152b99b
Bhasker Thodla <bthodla@gmail.com>
2020-03-09 11:46:59 -0400

    Testing ETL - first attempt
    Testing ETL - first attempt

--- data_modeling/project1/sql_queries.py
+++ data_modeling/project1/sql_queries.py
@@ -10,12 +10,12 @@ time_table_drop = "drop table time"
 
 songplay_table_create = ("""
     create table songplays (
-        songplay_id int, 
+        songplay_id serial not null primary key, 
         start_time timestamp, 
-        user_id int, 
+        user_id int references users(user_id), 
         level varchar, 
-        song_id int, 
-        artist_id int, 
+        song_id int references songs(song_id), 
+        artist_id int references artists(artist_id), 
         session_id int, 
         location varchar, 
         user_agent varchar
@@ -24,7 +24,7 @@ songplay_table_create = ("""
 
 user_table_create = ("""
     create table users (
-        user_id int, 
+        user_id int not null primary key, 
         first_name varchar, 
         last_name varchar, 
         gender varchar, 
@@ -34,7 +34,7 @@ user_table_create = ("""
 
 song_table_create = ("""
     create table songs (
-        song_id int, 
+        song_id int not null primary key, 
         title varchar, 
         artist_id int, 
         year int, 
@@ -44,7 +44,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
     create table artists (
-        artist_id int, 
+        artist_id int not null primary key, 
         name varchar, 
         location varchar, 
         latitude float, 
@@ -54,7 +54,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     create table time (
-        start_time timestamp, 
+        start_time timestamp not null primary key, 
         hour int, 
         day int, 
         week int, 
@@ -68,27 +68,27 @@ time_table_create = ("""
 
 songplay_table_insert = ("""
     insert into songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    values (1001, '2020-03-02 19:10:25-07', 101, 'Free', 10001, 2001, 51, 'New York, NY', 'Mozilla/5.0 (Windows NT 6.1)')
+    values (%d, %s, %d, %s, %d, %d, %d, %s, %s)
 """)
 
 user_table_insert = ("""
     insert into users (user_id, first_name, last_name, gender, level)
-    values (101, 'John', 'Smith', 'Male', 'Paid Subscription (Annual)')
+    values (%d, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
-    insert into songs (song_id, title, artist_id, year, duration
-    values (10001, 'Michelle', 2001, 1969, 222.1309)
+    insert into songs (song_id, title, artist_id, year, duration)
+    values (%d, %s, %d, %d, %f)
 """)
 
 artist_table_insert = ("""
     insert into artists (artist_id, name, location, latitude, longitude)
-    values (51, 'The Beatles', 'Liverpool, London', 51.5173469, 0.0831336)
+    values (%d, %s, %s, %f, %f)
 """)
 
 time_table_insert = ("""
     insert into time (start_time, hour, day, week, month, year, weekday)
-    values (1541990258.796, 2, 12, 46, 11, 2018, 'Mon')
+    values (%f, %d, %d, %d, %d, %d, %s)
 """)
 
 # FIND SONGS
