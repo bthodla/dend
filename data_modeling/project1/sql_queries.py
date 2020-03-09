6781e8432ea8e623931462608939e692e1305775
Bhasker Thodla <bthodla@gmail.com>
2020-03-09 14:40:21 -0400

    Modified Table Structures
    Modified Table Structures and changed primary keys in several dimension tables to varchar from int to conform to data

--- data_modeling/project1/sql_queries.py
+++ data_modeling/project1/sql_queries.py
@@ -12,10 +12,10 @@ songplay_table_create = ("""
     create table songplays (
         songplay_id serial not null primary key, 
         start_time timestamp, 
-        user_id int references users(user_id), 
+        user_id varchar references users(user_id), 
         level varchar, 
-        song_id int references songs(song_id), 
-        artist_id int references artists(artist_id), 
+        song_id varchar references songs(song_id), 
+        artist_id varchar references artists(artist_id), 
         session_id int, 
         location varchar, 
         user_agent varchar
@@ -24,7 +24,7 @@ songplay_table_create = ("""
 
 user_table_create = ("""
     create table users (
-        user_id int not null primary key, 
+        user_id varchar not null primary key, 
         first_name varchar, 
         last_name varchar, 
         gender varchar, 
@@ -34,7 +34,7 @@ user_table_create = ("""
 
 song_table_create = ("""
     create table songs (
-        song_id int not null primary key, 
+        song_id varchar not null primary key, 
         title varchar, 
         artist_id int, 
         year int, 
@@ -44,7 +44,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
     create table artists (
-        artist_id int not null primary key, 
+        artist_id varchar not null primary key, 
         name varchar, 
         location varchar, 
         latitude float, 
@@ -68,22 +68,22 @@ time_table_create = ("""
 
 songplay_table_insert = ("""
     insert into songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    values (%d, %s, %d, %s, %d, %d, %d, %s, %s)
+    values (%d, %s, %s, %s, %s, %s, %d, %s, %s)
 """)
 
 user_table_insert = ("""
     insert into users (user_id, first_name, last_name, gender, level)
-    values (%d, %s, %s, %s, %s)
+    values (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
     insert into songs (song_id, title, artist_id, year, duration)
-    values (%d, %s, %d, %d, %f)
+    values (%s, %s, %d, %d, %f)
 """)
 
 artist_table_insert = ("""
     insert into artists (artist_id, name, location, latitude, longitude)
-    values (%d, %s, %s, %f, %f)
+    values (%s, %s, %s, %f, %f)
 """)
 
 time_table_insert = ("""
@@ -102,5 +102,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
