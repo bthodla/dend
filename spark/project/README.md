913f937f90da49bf3d74942898b372befc29665e
Bhasker Thodla <bthodla@gmail.com>
2020-08-20 16:58:46 -0400

    Spark Project files

---
+++ spark/project/README.md
@@ -0,0 +1,73 @@
+# Data Lake Project
+
+## Project Scope
+
+With an increase in the user base and number of songs in its database, Sparkify wants to move this data into a data lke. The current data resides in Amazon S3 in JSON format and includes the following:
+
+- User Activity Logs
+- Song Metadata
+
+The scope of this project is to build an ETL pipeline that extracts this data from S3, process it using Spark and write the outputs back to S3 but in *paquet* format. As a part of the process, we should also split the data into facts and dimensions. And when writing all time-variant data, the data should be partitioned by time and any other appropriate attributes.
+
+## Schema Design
+
+The output will be classified into the following entities:
+
+- Facts
+    - songplays:
+        - songplay_id
+        - start_time
+        - user_id
+        - level
+        - session_id
+        - location
+        - user_agent
+        - song_id
+        - artist_id
+        - year
+        - month
+
+- Dimensions
+    - artists:
+        - artist_id
+        - artist_name
+        - artist_location
+        - artist_latitude
+        - artist_longitude
+    - songs
+        - title
+        - artist_id
+        - year
+        - duration
+    - time
+        - start_time
+        - hour
+        - day
+        - month
+        - year
+        - week
+        - weekday
+    - users
+        - user_id
+        - first_name
+        - last_name
+        - gender
+        - level
+        
+I think that this structure normalizes the data adequately for handling most of the analytical queries that may be thrown at the data lake. 
+
+For example, the single fact table contains references to primary keys of each of the dimension tables so that it may be joined with one or more of them.
+
+## Possible Queries
+
+Here is a list of some of the queries that may be answered by this design:
+
+1. Most popular songs
+2. Most popular artists
+3. Users who stream the maximum number of songs in a given period
+4. Artists with maximum number of songs
+5. User counts by gender
+6. Count of Free Vs. Paid users
+
+
+    
--- spark/project/etl_backup.py
+++ spark/project/etl_backup.py
@@ -3,17 +3,17 @@ from datetime import datetime
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import hour, dayofmonth, month, year, weekofyear, dayofweek, date_format
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 from pyspark.sql.functions import monotonically_increasing_id
 from pyspark.sql.types import StructType as St, StructField as Sf, DoubleType as Dbl, StringType as Str, IntegerType as Int, DateType as Dt, TimestampType
+
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-print (config)
-
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
 
 def create_spark_session():
     spark = SparkSession \
@@ -21,6 +21,7 @@ def create_spark_session():
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
     return spark
+
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
@@ -37,7 +38,7 @@ def process_song_data(spark, input_data, output_data):
     songs_table.write.partitionBy("year", "artist_id").parquet(output_data + 'songs/')
 
     # extract columns to create artists table
-    artists_columns = ["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]
+    artists_columns = ["artist_id", "name", "location", "latitude", "longitude"]
     
     # write artists table to parquet files
     artists_table = song_data_df.selectExpr(artists_columns).dropDuplicates()
@@ -68,8 +69,7 @@ def process_log_data(spark, input_data, output_data):
     log_data_df = log_data_df.withColumn("timestamp", get_timestamp(col("ts")))
     
     # create datetime column from original timestamp column
-    get_datetime = udf(lambda x: datetime.datetime(x), DatetimeType())
-    log_data_df = log_data_df.withColumn("start_time", get_datetime(col("ts")))
+    log_data_df = log_data_df.withColumn("start_time", get_timestamp(col("ts")))
     
     # extract columns to create time table
     log_data_df = log_data_df.withColumn("hour", hour("timestamp"))
@@ -85,24 +85,24 @@ def process_log_data(spark, input_data, output_data):
     time_table.write.partitionBy("year", "month").parquet(output_data + 'time/')
 
     # read in song data to use for songplays table
-    song_df = spark.sql("select distinct song_id, artist_id, artist_name from song_table_tmp_df")
+    song_df = spark.sql("select distinct song_id, artist_id, name from song_table_tmp_df")
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = log_data_df.join(song_df, song_df.artist_name == log_data_df.artist, "inner") \
-        .distinct() \
+    songplays_table = log_data_df.join(song_df, song_df.name == log_data_df.artist, "inner").distinct() \
         .select(col("start_time"), col("userId"), col("level"), col("sessionId"), col("location"), \
-            col("userAgent"), col("song_id"), col("artist_id"), col("year"), col("month")) \
+            col("userAget"), col("song_id"), col("artist_id")) \
         .withColumn("songplay_id", monotonically_increasing_id())
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table.write.partitionBy("year", "month").parquet(output_data + 'songplays_table/')
 
+
 def main():
     spark = create_spark_session()
 
-    song_input_data = "data/song_data/"
-    log_input_data = "data/log_data/"
-    output_data = "data/output_data/"
+    song_input_data = "home/workspace/data/song-data/"
+    log_input_data = "home/workspace/data/log-data/"
+    output_data = "home/workspace/data/output_data"
 
 
     # song_input_data = "s3a://udacity-dend/song-data/"
@@ -112,6 +112,7 @@ def main():
 
     process_song_data(spark, song_input_data, output_data)    
     process_log_data(spark, log_input_data, output_data)
+
 
 if __name__ == "__main__":
     main()
