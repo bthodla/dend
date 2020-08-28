9b862f859caa23e1d5baee34bb949abc0b7369e4
Bhasker Thodla <bthodla@gmail.com>
2020-08-28 17:23:39 -0400

    Create exercise6.py

---
+++ airflow/exercise6.py
@@ -0,0 +1,64 @@
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(credentials.access_key, credentials.secret_key))
+
+
+dag = DAG(
+    'lesson1.solution6',
+    start_date=datetime.datetime.now()
+)
+
+create_table = PostgresOperator(
+    task_id="create_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_task = PythonOperator(
+    task_id='load_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_data_to_redshift
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.LOCATION_TRAFFIC_SQL
+)
+
+create_table >> copy_task
+copy_task >> location_traffic_task
+
+# Conn ID: redshift
+# Conn Type: Postgres
+# Host: redshift-cluster.cdodedeqidlv.us-east-1.redshift.amazonaws.com:5439/bt-udacity
+# Schema: bt-udacity
+# Login: bthodla
+# Password: rsawsH0bb!t
+# Port: 5439
+# JDBC URL: jdbc:redshift://redshift-cluster.cdodedeqidlv.us-east-1.redshift.amazonaws.com:5439/bt-udacity
+# ODBC URL: Driver={Amazon Redshift (x64)}; Server=redshift-cluster.cdodedeqidlv.us-east-1.redshift.amazonaws.com; Database=bt-udacity
+
+# Redshift Cluster Identifier: redshift-bt-cluster-1
+
+# To compile a pythong source code file into python byte code file
+# python3 -m py_compile sql_statements.py
+
+# To compile all python source code files recursively starting from current directory
+# python3 -m compileall .
